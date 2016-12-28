a8d1bb014785dc05f20a834899e6af6ce341ad60
Balázs Szalkai <bszalkai@users.sourceforge.net>
2016-12-28 14:04:00 +0100

    Prepare release 0.6beta

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Satsuma")]
-[assembly: AssemblyCopyright("Copyright © 2013 Balázs Szalkai")]
+[assembly: AssemblyCopyright("Copyright © 2013-16 Balázs Szalkai")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.*")]
